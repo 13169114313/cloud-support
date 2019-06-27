9482df47543531b831df269469fe2939576bd865
DESKTOP-9OHMB5J\Chen <1332787671@qq.com>
2019-06-27 22:20:21 +0800

    提交基础服务

---
+++ cloud-admin/src/main/java/com/yi/cloud/admin/CloudAdminApplication.java
@@ -0,0 +1,16 @@
+package com.yi.cloud.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableAdminServer
+@SpringBootApplication
+@EnableEurekaClient
+public class CloudAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAdminApplication.class, args);
+    }
+}
---
+++ cloud-config/src/main/java/com/yi/cloud/config/ConfigApplication.java
@@ -0,0 +1,30 @@
+package com.yi.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * 服务启动类
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+    @Configuration
+    public class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll()
+                    .and().csrf().disable();
+        }
+    }
+}
